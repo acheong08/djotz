27d2993c0bfa9b071d2df75d134ba393e536386d
acheong08 <teapotv8@proton.me>
2024-02-11 22:12:38 +0000

    Unify to one test file

---
+++ src/tokenizer/Tokenizer_test.zig
@@ -0,0 +1,97 @@
+const Attributes = @import("Attributes.zig").Attributes;
+const AttributeEntry = @import("Attributes.zig").AttributeEntry;
+const std = @import("std");
+const assert = std.testing.expect;
+const token = @import("Token.zig");
+const TokenList = @import("TokenList.zig").TokenList;
+
+test "Attributes" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var gpalloc = gpa.allocator();
+
+    var a = Attributes.init(gpalloc);
+
+    try a.set("key1", "value1");
+    try assert(a.size() == 1);
+    try assert(std.mem.eql(u8, a.get("key1"), "value1"));
+
+    try a.append("key1", "value2");
+    try assert(a.size() == 1);
+    try assert(std.mem.eql(u8, a.get("key1"), "value1 value2"));
+
+    var b = Attributes.init(gpalloc);
+    try b.set("key2", "value3");
+
+    try a.mergeWith(&b);
+    var attributeEntryBuf = try gpalloc.alloc(AttributeEntry, a.size());
+    a.entries(attributeEntryBuf);
+    try assert(std.mem.eql(u8, attributeEntryBuf[0].key, "key1"));
+    try assert(std.mem.eql(u8, attributeEntryBuf[0].value, "value1 value2"));
+    gpalloc.free(attributeEntryBuf);
+
+    a.deinit();
+    b.deinit();
+
+    _ = gpa.deinit();
+}
+
+test "Token Range" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var allocator = gpa.allocator();
+    var ranges = token.Ranges.init(allocator);
+    defer ranges.deinit();
+
+    var range = token.Range{ .start = 0, .end = 10 };
+    try ranges.push(range);
+    try assert(ranges.ranges.items.len == 1);
+
+    range = token.Range{ .start = 10, .end = 20 };
+    try ranges.push(range);
+    try assert(ranges.ranges.items.len == 1);
+
+    range = token.Range{ .start = 15, .end = 25 };
+    try ranges.push(range);
+    try assert(ranges.ranges.items.len == 2);
+}
+
+test "Token" {
+    var tok = token.Token(isize).init(null, 0, 0);
+    defer tok.deinit();
+
+    tok.start = 0;
+    tok.end = 3;
+    try assert(tok.length() == 3);
+
+    try assert(tok.isDefault());
+
+    const document = "### Hello, World!";
+
+    try assert(std.mem.eql(u8, tok.bytes(document), "###"));
+
+    try assert(tok.prefixLength(document, '#') == 3);
+}
+
+test "TokenList" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var allocator = gpa.allocator();
+
+    var tokens = TokenList(isize).init(allocator);
+    defer tokens.deinit();
+
+    try tokens.push(token.Token(isize).init(1, 0, 1));
+    try tokens.push(token.Token(isize).init(2, 10, 11));
+
+    var expected = try allocator.alloc(token.Token(isize), 3);
+    defer allocator.free(expected);
+    expected[0] = token.Token(isize).init(1, 0, 1);
+    expected[1] = token.Token(isize).init(null, 1, 10);
+    expected[2] = token.Token(isize).init(2, 10, 11);
+
+    try assert(tokens.items.items.len == 3);
+
+    for (0..tokens.items.items.len) |i| {
+        try std.testing.expectEqual(expected[i], tokens.items.items[i]);
+    }
+}
