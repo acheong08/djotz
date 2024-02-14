47cdeaa009762891e004bf5935a651f6a8cd76de
acheong08 <teapotv8@proton.me>
2024-02-14 15:48:44 +0000

    BlockToken initial commit

---
+++ src/djot_tokenizer/BlockToken.zig
@@ -0,0 +1,14 @@
+const tokenizer = @import("../tokenizer/TextReader.zig");
+
+pub const NotSpaceByteMask = tokenizer.SpaceNewLineByteMask.Negate();
+pub const NotBracketByteMask = tokenizer.ByteMask.init("]").Negate();
+pub const ThematicBreakByteMask = tokenizer.ByteMask.init(" \t\n*-");
+pub const DigitByteMask = tokenizer.ByteMask.init("0123456789");
+pub const LowerAlphaByteMask = tokenizer.ByteMask.init("abcdefghijklmnopqrstuvwxyz");
+pub const UpperAlphaByteMask = tokenizer.ByteMask.init("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+pub const AttributeTokenMask = tokenizer.Union([]tokenizer.ByteMask{
+    DigitByteMask,
+    LowerAlphaByteMask,
+    UpperAlphaByteMask,
+    tokenizer.ByteMask.init("-_:"),
+});
