ccb23afe967f2f3c8b04e89ad27a52e6b3feda30
acheong08 <teapotv8@proton.me>
2024-02-21 13:03:42 +0000

    BuildInlineDjotTokens snapshot (untested)

---
+++ src/djot_tokenizer/DjotTokenizer.zig
@@ -0,0 +1,135 @@
+const std = @import("std");
+const Range = @import("../tokenizer/Token.zig").Range;
+const djotToken = @import("Token.zig");
+const Tokens = djotToken.tokens;
+const Opposite = djotToken.opposite;
+const Token = @import("../tokenizer/Token.zig").Token(Tokens);
+const TokenStack = @import("../tokenizer/TokenStack.zig").TokenStack(Tokens);
+const TextReader = @import("../tokenizer/TextReader.zig").TextReader;
+const matchInlineToken = @import("InlineToken.zig").matchInlineToken;
+const matchDjotAttribute = @import("Attributes.zig").matchDjotAttribute;
+const Attributes = @import("Attributes.zig").Attributes;
+const inlineTokens = @import("InlineToken.zig");
+
+pub fn BuildInlineDjotTokens(allocator: std.mem.Allocator, doc: []const u8, parts: std.ArrayList(Range)) ![]Tokens {
+    if (parts.items.len == 0) {
+        parts.append(Range{ .start = 0, .end = doc.len });
+    }
+    var tokenStack = TokenStack.init(allocator);
+    defer tokenStack.deinit();
+    const leftDocumentPosition = parts.items[0].start;
+    const rightDocumentPosition = parts.items[parts.items.len - 1].end;
+    tokenStack.openLevelAt(Token.init(Tokens.ParagraphBlock, leftDocumentPosition, leftDocumentPosition));
+
+    var inlineParsingLoop = true;
+
+    var reader = TextReader.init(doc);
+    var state = parts.items[0].start;
+    tokenStack.lastLevel().?.fillUntil(parts.items[0].start, Tokens.Ignore);
+    for (parts.items) |part| {
+        if (inlineParsingLoop == true) {
+            reader = TextReader.init(doc);
+            state = part.start;
+            tokenStack.lastLevel().?.fillUntil(parts.items[0].start, Tokens.Ignore);
+        }
+        inlineParsingLoop = true;
+        while (!reader.isEmpty(state)) {
+            const openInline = tokenStack.lastLevel().?.firstOrDefault();
+            const openInlineType = openInline.tokenType;
+            const lastInline = tokenStack.lastLevel().?.lastOrDefault();
+
+            if (openInlineType == Tokens.VerbatimInline) {
+                const next = try matchInlineToken(reader, state, Opposite(Tokens.VerbatimInline)) orelse {
+                    state += 1;
+                    continue;
+                };
+                const openToken = reader.select(openInline.start, openInline.end);
+                const closeToken = reader.select(state, next);
+                if (std.mem.trimLeft(u8, openToken, "$") != closeToken) {
+                    state = next;
+                    continue;
+                }
+                tokenStack.closeLevelAt(Token.init(Opposite(Tokens.VerbatimInline), state, next));
+                state = next;
+                continue;
+            }
+            var attributes = Attributes.init(allocator);
+            if (try matchDjotAttribute(reader, state, attributes)) |next| {
+                var tok = Token.init(Tokens.Attribute, state, next);
+                tok.attributes = attributes;
+                tokenStack.lastLevel().?.push(tok);
+                state = next;
+                continue;
+            }
+            if (inlineTokens.InlineTokenStartSymbol.Has(reader.doc[state])) {
+                state += 1;
+                continue;
+            }
+            // EscapedSymbolInline / SmartSymbolInline is non-paired tokens - so we should treat it separately
+            inline for ([_]Tokens{ Tokens.EscapedSymbolInline, Tokens.SmartSymbolInline }) |tokenType| {
+                if (try matchInlineToken(reader, state, tokenType)) |next| {
+                    tokenStack.lastLevel().?.push(Token.init(tokenType, state, next));
+                    state = next;
+                    inlineParsingLoop = false;
+                    break;
+                }
+            }
+            inline for ([_]Tokens{
+                Tokens.RawFormatInline,
+                Tokens.VerbatimInline,
+                Tokens.ImageSpanInline,
+                Tokens.LinkUrlInline,
+                Tokens.LinkReferenceInline,
+                Tokens.AutolinkInline,
+                Tokens.EmphasisInline,
+                Tokens.StrongInline,
+                Tokens.HighlightedInline,
+                Tokens.SubscriptInline,
+                Tokens.SuperscriptInline,
+                Tokens.InsertInline,
+                Tokens.DeleteInline,
+                Tokens.FootnoteReferenceInline,
+                Tokens.SpanInline,
+                Tokens.SymbolsInline,
+                Tokens.PipeTableSeparator,
+            }) |tokenType| {
+                const next = matchInlineToken(reader, state, Opposite(tokenType));
+                const forbidClose = ((tokenType == Tokens.EmphasisInline and lastInline.tokenType == Tokens.EmphasisInline) or (tokenType == Tokens.StringInline and lastInline.tokenType == Tokens.StrongInline)) and lastInline.end == state;
+                if (!forbidClose and next != null and try tokenStack.popForgetUntil(tokenType)) {
+                    tokenStack.closeLevelAt(Token.init(Opposite(tokenType), state, next));
+                    state = next;
+                    inlineParsingLoop = false;
+                    break;
+                }
+                if (tokenType == Tokens.LinkReferenceInline or tokenType == Tokens.LinkUrlInline and lastInline.tokenType != Opposite(Tokens.SpanInline) and lastInline.tokenType != Opposite(Tokens.ImageSpanInline)) {
+                    continue;
+                }
+                if (matchInlineToken(reader, state, tokenType)) |nnext| {
+                    attributes = Attributes.init(allocator);
+                    const token = reader.doc[state..nnext];
+                    if (tokenType == Tokens.VerbatimInline) {
+                        if (std.mem.startsWith(u8, token, "$$")) {
+                            attributes.set(djotToken.blockKeys.DisplayMathKey, "");
+                        } else if (std.mem.startsWith(u8, token, "$")) {
+                            attributes.set(djotToken.blockKeys.InlineMathKey, "");
+                        }
+                    }
+                    const tok = Token.init(tokenType, state, nnext);
+                    tok.attributes = attributes;
+                    tokenStack.openLevelAt(tok);
+                    state = nnext;
+                    inlineParsingLoop = false;
+                    break;
+                }
+            }
+            state += 1;
+        }
+    }
+    if (tokenStack.lastLevel().?.firstOrDefault().tokenType == Tokens.VerbatimInline) {
+        tokenStack.closeLevelAt(Token.init(Opposite(Tokens.VerbatimInline), rightDocumentPosition, rightDocumentPosition));
+    }
+    tokenStack.popForgetUntil(Tokens.ParagraphBlock);
+    tokenStack.closeLevelAt(Token.init(Tokens.ParagraphBlock, rightDocumentPosition, rightDocumentPosition));
+    const tokens = tokenStack.lastLevel().?.*;
+    return tokens[1 .. tokens.len - 1];
+}
