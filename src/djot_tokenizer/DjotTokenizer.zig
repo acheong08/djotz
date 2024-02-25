3ab13ed32e3a11431b0a55fa252c7c9b12bd96d9
acheong08 <teapotv8@proton.me>
2024-02-25 18:21:14 +0000

    BuildDjotTokens WIP snapshot

--- src/djot_tokenizer/DjotTokenizer.zig
+++ src/djot_tokenizer/DjotTokenizer.zig
@@ -1,17 +1,21 @@
 const std = @import("std");
-const Range = @import("../tokenizer/Token.zig").Range;
 const djotToken = @import("Token.zig");
 const Tokens = djotToken.tokens;
 const Opposite = djotToken.opposite;
 const Token = @import("../tokenizer/Token.zig").Token(Tokens);
+const Range = @import("../tokenizer/Token.zig").Range;
 const TokenStack = @import("../tokenizer/TokenStack.zig").TokenStack(Tokens);
 const TextReader = @import("../tokenizer/TextReader.zig").TextReader;
 const matchInlineToken = @import("InlineToken.zig").MatchInlineToken;
 const matchDjotAttribute = @import("Attributes.zig").matchDjotAttribute;
 const Attributes = @import("Attributes.zig").Attributes;
 const inlineTokens = @import("InlineToken.zig");
+const TokenList = @import("../tokenizer/TokenList.zig").TokenList(Tokens);
+const LineTokenizer = @import("../tokenizer/LineTokenizer.zig").LineTokenizer;
 
-pub fn BuildInlineDjotTokens(allocator: std.mem.Allocator, doc: []const u8, parts: *std.ArrayList(Range), tokens: *std.ArrayList(Token)) !void {
+pub fn BuildInlineDjotTokens(allocator: std.mem.Allocator, doc: []const u8, cparts: std.ArrayList(Range), tokens: *std.ArrayList(Token)) !void {
+    var parts = try cparts.clone();
+    defer parts.deinit();
     if (parts.items.len == 0) {
         try parts.append(Range{ .start = 0, .end = doc.len });
     }
@@ -134,3 +138,67 @@ pub fn BuildInlineDjotTokens(allocator: std.mem.Allocator, doc: []const u8, part
     }
     _ = tokens.pop();
 }
+
+pub const BuildDjotTokens = struct {
+    allocator: std.mem.Allocator,
+    lineTokenizer: LineTokenizer,
+    inlineParts: std.ArrayList(Range),
+    blockLineOffset: std.ArrayList(usize),
+    blockTokenOffset: std.ArrayList(usize),
+
+    blockTokens: std.ArrayList(Token),
+    finalTokens: std.ArrayList(Token),
+
+    pub fn init(allocator: std.mem.Allocator, doc: []const u8) !BuildDjotTokens {
+        var self = BuildDjotTokens{};
+        self.allocator = allocator;
+        self.lineTokenizer = LineTokenizer.init(doc);
+        self.inlineParts = std.ArrayList(Range).init(allocator);
+        self.blockLineOffset = std.ArrayList(usize).init(allocator);
+        self.blockTokenOffset = std.ArrayList(usize).init(allocator);
+
+        self.blockTokens = std.ArrayList(Token).init(allocator);
+        self.finalTokens = std.ArrayList(Token).init(allocator);
+        self.blockTokens.append(Token.init(Tokens.DocumentBlock, 0, 0));
+        self.finalTokens.append(Token.init(Tokens.DocumentBlock, 0, 0));
+        return self;
+    }
+    fn popMetadata(self: *BuildDjotTokens) void {
+        self.blockLineOffset.pop();
+        self.blockTokenOffset.pop();
+        self.blockTokens.pop();
+    }
+    fn openBlockLevel(self: *BuildDjotTokens, token: Token) !void {
+        try self.finalTokens.append(token);
+        try self.blockTokenOffset.append(self.blockTokens.items.len - 1);
+        try self.blockTokens.append(token);
+    }
+
+    fn closeBlockLevelsUntil(self: *BuildDjotTokens, start: usize, end: usize, level: usize) !void {
+        if (self.inlineParts.items.len != 0 and self.blockTokens.getLast().tokenType == Tokens.CodeBlock) {
+            for (self.inlineParts.items) |part| {
+                try self.finalTokens.append(Token.init(Tokens.None, part.start, part.end));
+            }
+            self.inlineParts.clearAndFree();
+        } else if (self.inlineParts.items.len != 0) {
+            var inlineToks = std.ArrayList(Token).init(self.allocator);
+            defer inlineToks.deinit();
+            try BuildInlineDjotTokens(self.allocator, self.inlineParts, &inlineToks);
+            try self.finalTokens.appendSlice(inlineToks.items);
+            self.inlineParts.clearAndFree();
+        }
+        var i = self.blockTokens.items.len - 1;
+        while (i > level) : (i -= 1) {
+            try self.finalTokens.append(Token.init(Opposite(self.blockTokens.items[i].tokenType), start, end));
+            const delta = self.finalTokens.items.len - 1 - self.blockTokenOffset.items[i];
+            self.finalTokens.items[self.blockTokenOffset.items[i]].jumpToPair = delta;
+            self.finalTokens.items[self.finalTokens.items.len - 1].jumpToPair = -delta;
+            self.popMetadata();
+        }
+    }
+
+    pub fn build(self: *BuildDjotTokens, doc: []const u8) !?TokenList {
+        _ = doc;
+        _ = self;
+    }
+};
